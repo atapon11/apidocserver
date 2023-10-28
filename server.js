46ed04002879af1ee6906feb0706de758352d868
atapon11 <634259017@webmail.npru.ac.th>
2023-10-29 00:03:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = 5000;
 
 // เพิ่ม middleware สำหรับการกำหนด CORS headers
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://muddy-dog-bedclothes.cyclic.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
